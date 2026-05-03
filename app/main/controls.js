48a08c10f4700576a54cd2a4d7e22d655bed3f80
AZDriveai <zyz151224@gmail.com>
2026-05-04 01:26:48 +0400

    Update controls.js

--- app/main/controls.js
+++ app/main/controls.js
@@ -1,1 +1,10 @@
+const { ipcMain, BrowserWindow } = require("electron");
+
+ipcMain.on("win:min", e => {
+  BrowserWindow.fromWebContents(e.sender).minimize();
+});
+
+ipcMain.on("win:close", e => {
+  BrowserWindow.fromWebContents(e.sender).close();
+});
 
